559408f3cfeadd58eab735b517fa02d8040b9e99
germanrogu <german.rodriguezgu@gmail.com>
2024-12-29 08:13:41 -0500

    Add product module and add configuration in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './domain/product.entity';
+import { Transaction } from './domain/transaction.entity';
+import { Customer } from './domain/customer.entity';
+import { Delivery } from './domain/delivery.entity';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Product, Transaction, Customer, Delivery],
+      synchronize: false,
+    }),
+    TypeOrmModule.forFeature([Product, Transaction, Customer, Delivery]),
+    ProductsModule,
+  ],
 })
 export class AppModule {}
